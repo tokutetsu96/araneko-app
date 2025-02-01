f2f9adc10429b02886c93e42856a8345cf86b8ad
tokutetsu96 <tokunagatetsuya96@gmail.com>
2025-02-01 22:14:40 +0900

    ログインが無いときはログイン画面に

--- app/middleware.ts
+++ app/middleware.ts
@@ -3,19 +3,16 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
-
     if (!req.nextauth?.token) {
       return NextResponse.redirect(new URL("/auth/signin", req.url));
     }
   },
   {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
     pages: {
       signIn: "/auth/signin",
     },
   }
 );
-
-// // 認証が必要なページを指定
-// export const config = {
-//   matcher: ["/dashboard", "/profile"], // 例: /dashboard と /profile を保護
-// };
