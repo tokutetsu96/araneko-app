daae681bec2f4a0d29892840196d20a1bc56066f
tokutetsu96 <tokunagatetsuya96@gmail.com>
2025-02-13 20:32:28 +0900

    エラー解消

---
+++ prisma/migrations/20250213113146_init/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OpggSummoner" DROP CONSTRAINT "OpggSummoner_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
